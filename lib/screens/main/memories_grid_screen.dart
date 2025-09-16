04a9267e051b95cda236bf5a47f7fbe85c91f9de
Pedro Henrique <pedibr5@gmail.com>
2025-09-16 13:34:05 -0300

    Tela inicial

--- lib/screens/main/memories_grid_screen.dart
+++ lib/screens/main/memories_grid_screen.dart
@@ -9,7 +9,6 @@ class MemoriesGridScreen extends StatefulWidget {
 
   @override
   State<MemoriesGridScreen> createState() => _MemoriesGridScreenState();
-
 }
 
 class _MemoriesGridScreenState extends State<MemoriesGridScreen> {
@@ -18,39 +17,52 @@ class _MemoriesGridScreenState extends State<MemoriesGridScreen> {
     super.initState();
     loadMemories();
   }
+
   void loadMemories() {
     WidgetsBinding.instance.addPostFrameCallback((_) {
       Provider.of<MemoryProvider>(context, listen: false).getMemories();
     });
   }
+
   @override
   Widget build(BuildContext context) {
     final _memoriesProvider = Provider.of<MemoryProvider>(context);
     return Scaffold(
-      body: _memoriesProvider.isLoading
-          ? const Center(child: CircularProgressIndicator())
-          : MasonryGridView.builder(
-        gridDelegate:
-        const SliverSimpleGridDelegateWithFixedCrossAxisCount(
-          crossAxisCount: 2,
-        ),
-        mainAxisSpacing: 8,
-        crossAxisSpacing: 8,
-        padding: const EdgeInsets.all(12),
-        itemCount: _memoriesProvider.memories.length,
-        itemBuilder: (context, index) {
-          final memory = _memoriesProvider.memories[index];
-          return ClipRRect(
-            borderRadius: BorderRadius.circular(12),
-            child: Image.network(
-              memory.photoUrl,
-              fit: BoxFit.cover,
-            ),
-          );
-        },
-      ),
+      appBar: AppBar(title: Text("Home"), automaticallyImplyLeading: false,),
+      body:
+          _memoriesProvider.isLoading
+              ? const Center(child: CircularProgressIndicator())
+              : SingleChildScrollView(
+                child: Column(
+                  children: [
+                    Text("Minhas Memórias", style: TextStyle(fontSize: 24),),
+                    SizedBox(
+                      height: MediaQuery.of(context).size.height * 0.7,
+                      child: MasonryGridView.builder(
+                        physics: NeverScrollableScrollPhysics(),
+                        gridDelegate:
+                            const SliverSimpleGridDelegateWithFixedCrossAxisCount(
+                              crossAxisCount: 2,
+                            ),
+                        mainAxisSpacing: 8,
+                        crossAxisSpacing: 8,
+                        padding: const EdgeInsets.all(12),
+                        itemCount: _memoriesProvider.memories.length,
+                        itemBuilder: (context, index) {
+                          final memory = _memoriesProvider.memories[index];
+                          return ClipRRect(
+                            borderRadius: BorderRadius.circular(12),
+                            child: Image.network(memory.photoUrl, fit: BoxFit.cover),
+                          );
+                        },
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+      floatingActionButton: FloatingActionButton(onPressed: () {
+        // TODO: Navigate to AddMemoryScreen
+      },),
     );
   }
-
-
 }
